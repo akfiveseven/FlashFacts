c38dbcd9487d0f682486435314f58597a86ba098
aymar <44442172+aymar1400@users.noreply.github.com>
2023-06-16 18:25:56 -0700

    almost done

--- src/Select.js
+++ src/Select.js
@@ -1,24 +1,52 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
-import './style.css'
+import { Link } from 'react-router-dom';
+import './style.css';
+import CardList from './CardList';
+
 export default function Select({ setList }) {
+  const sets = JSON.parse(localStorage.getItem('flashcardSets')) || [];
 
-    const sets = JSON.parse(localStorage.getItem('flashcardSets')) || []; // getting sets data in local storage
+  const [selectedOption, setSelectedOption] = useState('');
+  const [showResult, setShowResult] = useState(false); // State variable to control rendering
 
-    const [selectedOption, setSelectedOption] = useState('');
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value);
+    setShowResult(false); // Reset the showResult state when the option changes
+  };
 
-    const handleChange = (event) => {
-        setSelectedOption(event.target.value);
-    }
-
-    return (
+  const handleViewSet = () => {
+    const selectedSet = sets.find((set) => set.name === selectedOption);
+    
+    if (selectedSet) {
+      return (
         <div>
-            <select value={selectedOption} onChange={handleChange}>
-                <option value="">Select an option</option>
-                <option value="option1">{sets[0].name}</option>
-            </select>
+          <CardList questions={selectedSet.cards} />
         </div>
+      );
+    } else {
+        return null;
+    }
+  };
+  
+  
 
+  const handleClick = () => {
+    setShowResult(true); // Set showResult to true when the button is clicked
+  };
 
-    );
-} 
+  return (
+    <div>
+      <select value={selectedOption} onChange={handleChange}>
+        <option value="">Select an option</option>
+        {sets.map((set) => (
+          <option key={set.id} value={set.name}>
+            {set.name}
+          </option>
+        ))}
+      </select>
+
+      <button onClick={handleClick}>Show Result</button> {/* Button to trigger rendering */}
+      {showResult && handleViewSet()} {/* Render the result of handleViewSet when showResult is true */}
+    </div>
+  );
+}
